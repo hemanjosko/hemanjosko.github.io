00e96748aca634b699e472514c8397f207cff3d0
hemanjosko <thehimanshuone@gmail.com>
2020-06-14 03:21:01 +0530

    hindi test

--- myna/container/js/index.js
+++ myna/container/js/index.js
@@ -2,5 +2,8 @@ $( document ).ready(function() {
 	var queryString = window.location.search;
 	var urlParams = new URLSearchParams(queryString);
 	var lang = urlParams.get('lang')
-	alert(lang);
+	if(lang=="HN"){
+	$(".navbar-brand").html("मैना");
+	}
 });
+
