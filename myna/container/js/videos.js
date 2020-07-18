f207518508f5b98c83e39c407266b1a0f928b8cc
hemanjosko <thehimanshuone@gmail.com>
2020-07-18 17:13:25 +0530

    videos updates

--- myna/container/js/videos.js
+++ myna/container/js/videos.js
@@ -2,17 +2,17 @@ var queryString = window.location.search;
 var urlParams = new URLSearchParams(queryString);
 var lang = urlParams.get('lang');
 var latest_list = ["be-TpNWMCc8", "pukcy-8FbPg", "0gdy2yerZNg", "v8_iG56qLJk", "YmmaBS3_Ohc", "RIr-xKgQlMI", "P2YPG8PO9JU"];
-var organic_list = ["b49_XnutptQ", "Pj4S_OQEDUU", "2qiNKen-rm0", "G_uJ-aXXTEY", "CSxygxIRGZ8"];
-var invention_list = ["ZbiP2VH-SW8", "d5PTKHRw2FQ", "lzGE4mBRPK0", "lt1vnLJu1GU", "X-p4P0SQytM", "KAV0w-UIobI"];
+var organic_list = ["b49_XnutptQ", "Pj4S_OQEDUU", "2qiNKen-rm0", "G_uJ-aXXTEY", "CSxygxIRGZ8", "dkZsIpKhdBs"];
+var invention_list = ["d5PTKHRw2FQ", "lzGE4mBRPK0", "lt1vnLJu1GU", "X-p4P0SQytM", "KAV0w-UIobI"];
 if(lang!="" && lang=="MH"){
-    latest_list = ["c0XdPXa3T1I", "pukcy-8FbPg", "0gdy2yerZNg", "v8_iG56qLJk", "YmmaBS3_Ohc", "RIr-xKgQlMI", "P2YPG8PO9JU"];
-    organic_list = ["b49_XnutptQ", "Pj4S_OQEDUU", "2qiNKen-rm0", "G_uJ-aXXTEY", "CSxygxIRGZ8"];
-    invention_list = ["ZbiP2VH-SW8", "d5PTKHRw2FQ", "lzGE4mBRPK0", "lt1vnLJu1GU", "X-p4P0SQytM", "KAV0w-UIobI"];
+    latest_list = ["c0XdPXa3T1I", "vsyrZozTbXI", "yEVcXdRfGFM", "2_PODAkg64M", "_SC6_a4SmsU", "AnKFHadJsko", "7bxmDDDxy9w"];
+    organic_list = ["80v4K50MUoI", "algLQ3k6IzM", "dCMvqR7ew2Q", "3E_YChXnoD8", "972iwZJSDC4"];
+    invention_list = ["d5PTKHRw2FQ", "lzGE4mBRPK0", "lt1vnLJu1GU", "X-p4P0SQytM", "KAV0w-UIobI"];
 }
 if(lang!="" && lang=="PJ"){
-    latest_list = ["o90_Bx3yIZA", "pukcy-8FbPg", "0gdy2yerZNg", "v8_iG56qLJk", "YmmaBS3_Ohc", "RIr-xKgQlMI", "P2YPG8PO9JU"];
-    organic_list = ["b49_XnutptQ", "Pj4S_OQEDUU", "2qiNKen-rm0", "G_uJ-aXXTEY", "CSxygxIRGZ8"];
-    invention_list = ["ZbiP2VH-SW8", "d5PTKHRw2FQ", "lzGE4mBRPK0", "lt1vnLJu1GU", "X-p4P0SQytM", "KAV0w-UIobI"];
+    latest_list = ["o90_Bx3yIZA", "PIohT3Wnn3k", "Y6wN74ElIys", "VShXHRbynwc", "uNrBeZJnAKY", "46-5yGVU_Yc", "_AHNO3NLjbY", "zX7EN3ppm_Q"];
+    organic_list = ["Dv736bhwZxU", "cYwIjAMxqPg", "LLOSiCNu11w", "ow7Pl21P5K0", "xIHT3-nh4SU", "IJWjl3mnhLQ", "fkkIE_8-LtU", "Ojx5R76C4H0"];
+    invention_list = ["HVPPsaI8tbE", "d5PTKHRw2FQ", "lzGE4mBRPK0", "lt1vnLJu1GU", "X-p4P0SQytM", "KAV0w-UIobI"];
 }
 var g_organic = {
 	"EN": "Organic",
